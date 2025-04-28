471b5e606d13640709ada15bb3e3c58fd4b03bce
Azat201003 <zanelane87@gmail.com>
2025-04-28 22:18:47 +0400

    summary manager start (sending only)

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO
+
+- add length token param
+[x] realize auth in users service
+- create groups
+- add function to summary service **"GetRecomendsById"**
+- add param to summary manager to (up/down)load size of chunk
+- add permissions to summary_manager
+- add creator to summary
---
+++ src/summary_manager/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"summager-service/server"
+
+	config "github.com/Azat201003/eduflow_service_api/config"
+	"github.com/redis/go-redis/v9"
+
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/Azat201003/eduflow_service_api/gen/go/summager"
+
+	"google.golang.org/grpc"
+)
+
+const SERVICE_ID = 0
+
+func main() {
+	// config getting
+	conf, err := config.GetConfig("../../config.yaml")
+	if err != nil {
+		log.Fatalf("Error with getting config: %v", err)
+	}
+	service_config, err := conf.GetServiceById(2)
+	if err != nil {
+		log.Fatalf("Error with finding condfig: %v", err)
+	}
+
+	// connecting redis
+	redis_client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%v:%v", conf.Redis.Host, conf.Redis.Port),
+		Username: service_config.RedisConnect.User,
+		Password: service_config.RedisConnect.Password, // No password set
+		DB:       int(service_config.RedisConnect.DB),  // Use default DB
+	})
+
+	// starting server
+	lis, err := net.Listen("tcp", fmt.Sprintf("%v:%d", service_config.Host, service_config.Port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterSummaryManagerServiceServer(grpcServer, server.NewServer(redis_client))
+
+	fmt.Println(lis.Addr())
+	grpcServer.Serve(lis)
+}
